f0f940f3927b7175bb89a7ba94633f34d173a79d
machv <vladimir@mach.im>
2012-02-08 10:08:27 +0000

    base64 to bitmap

--- LeafService/LeafService.cs
+++ LeafService/LeafService.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using System.Drawing;
+using System.IO;
 
 namespace LeafService
 {
@@ -16,9 +18,18 @@ namespace LeafService
 
         public RecognizedLeaf Recognize(string picture, int noAnswers = 3)
         {
-			// prijima obrazek jako string zakodovany v base64, ten se rozkoduje do Bitmapy, vypocita deskriptor, a hleda
-			//http://stackoverflow.com/questions/6807283/transferring-byte-array-from-soap-service-to-android
-            throw new NotImplementedException();
+            // prijima obrazek jako string zakodovany v base64, ten se rozkoduje do Bitmapy, vypocita deskriptor, a hleda
+            //http://stackoverflow.com/questions/6807283/transferring-byte-array-from-soap-service-to-android
+
+            byte[] imageBytes = Convert.FromBase64String(picture);
+
+            MemoryStream ms = new MemoryStream(imageBytes);
+            Bitmap img = (Bitmap)Image.FromStream(ms);
+            ms.Close();
+
+
+            //na tenhle img zavolat rozpozavani
+            
         }
 
         public bool Learn(Descriptor[] descriptors, string picture, string treeName, SenderInfo senderInfo)
